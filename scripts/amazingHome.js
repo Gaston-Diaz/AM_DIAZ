d7708eb70d51baa894fe0c67a128cdc162119abf
Gaston-Diaz <gastondiaz.lp@gmail.com>
2023-02-26 15:22:15 -0300

    Se agrego el script de las cards al home

--- scripts/amazingHome.js
+++ scripts/amazingHome.js
@@ -171,3 +171,27 @@ const data = {
       }
     ]
   }
+
+  let fragmento = document.createDocumentFragment();
+  function cards(array, containerCard){
+    for(let event of array.events){
+      let div = document.createElement("div");
+      div.className = "cardDiv"
+      div.innerHTML += `
+      <div class="card" style="width: 18rem;">
+      <img src="${event.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">${event.description}</p>
+      </div>
+      <div class="card-footer">
+        Price ${event.price}
+        <a href="./details.html" class="btn btn-primary">details</a>
+      </div>
+    </div>
+      `
+      fragmento.appendChild(div);
+    }
+    containerCard.appendChild(fragmento);
+  }
+
+  cards(data,containerCard);
