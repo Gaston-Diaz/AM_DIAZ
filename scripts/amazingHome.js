d3e3832d9ef4e6a8aa4fa663b21e68fa1134955b
Gaston-Diaz <gastondiaz.lp@gmail.com>
2023-03-08 20:11:41 -0300

    Se modificaron los archivos index y amazingHome.js

--- scripts/amazingHome.js
+++ scripts/amazingHome.js
@@ -1,6 +1,7 @@
   import data from './data.js'
-
+/*
   let fragmento = document.createDocumentFragment();
+
   function cards(array, containerCard){
     for(let event of array.events){
       let div = document.createElement("div");
@@ -22,6 +23,36 @@
     containerCard.appendChild(fragmento);
   }
   cards(data,containerCard);
+*/
 
+let $containerCard = document.getElementById('containerCard')
+let fragment = document.createDocumentFragment();
+
+const crearCards = (array, containerCard) =>{
+  $containerCard.innerHTML = ""
+  let eventos = array.events
+  eventos.forEach(event => {
+
+    let card = document.createElement('div')
+    card.className = `cardDiv ${event.name.toLowerCase()}`
+    card.innerHTML = `
+    <div style="width: 18rem;" class="card">
+      <img src="${event.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <p class="card-text">${event.description}</p>
+      </div>
+      <div class="card-footer">
+        Price ${event.price}
+        <a href="./details.html" class="btn btn-primary">details</a>
+      </div>
+    </div>
+    `
+    fragment.appendChild(card);
+
+  });
+  containerCard.appendChild(fragment);
+}
+
+crearCards(data, $containerCard)
 
 
