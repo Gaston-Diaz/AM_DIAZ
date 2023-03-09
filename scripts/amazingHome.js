07942f45e3333d63e08d05063a7d850f8064badb
Gaston <gastondiaz.lp@gmail.com>
2023-03-09 16:11:42 -0300

    Se trabajo sobre los checkbox

--- scripts/amazingHome.js
+++ scripts/amazingHome.js
@@ -1,29 +1,4 @@
   import data from './data.js'
-/*
-  let fragmento = document.createDocumentFragment();
-
-  function cards(array, containerCard){
-    for(let event of array.events){
-      let div = document.createElement("div");
-      div.className = "cardDiv"
-      div.innerHTML += `
-      <div style="width: 18rem;" class="card">
-      <img src="${event.image}" class="card-img-top" alt="...">
-      <div class="card-body">
-        <p class="card-text">${event.description}</p>
-      </div>
-      <div class="card-footer">
-        Price ${event.price}
-        <a href="./details.html" class="btn btn-primary">details</a>
-      </div>
-    </div>
-      `
-      fragmento.appendChild(div);
-    }
-    containerCard.appendChild(fragmento);
-  }
-  cards(data,containerCard);
-*/
 
 let $containerCard = document.getElementById('containerCard') // capturar el elemento conteiner de las cards
 let fragment = document.createDocumentFragment();
@@ -73,16 +48,27 @@ const capturarCategorias = (array) =>{ //hago un array con las categorias de eve
 }
 let categorias = capturarCategorias(data.events)
 const crearCheckbox = (array,checkboxDiv) =>{ //se crean las checkbox dinamicas 
-  let text ="";
-  array.forEach(element => text +=`
+  let aux ="";
+  array.forEach(element => aux +=`
   <div class="form-check form-check-inline">
     <input class="form-check-input" type="checkbox" id=${element} value=${element}>
     <label class="form-check-label" for=${element}>${element}</label>
   </div>
   `);
-  checkboxDiv.innerHTML = text;
+  checkboxDiv.innerHTML = aux;
 }
 crearCheckbox(categorias,$checkboxDiv)
+
+const filtroCheckbox = (array, value) => { // se muestarn las cards que cumplen con la categoria del checkbox
+  let arrayFiltrado = array.filter(evento => evento.category.toLowerCase().includes(value))
+  return arrayFiltrado 
+}
+
+$checkboxDiv.addEventListener('change', (e)=>{
+  console.log(e.target.value)
+  let nArray = filtroCheckbox(data.events,e.target.value)
+  console.log(nArray)
+})
 
 $inputSearch.addEventListener('keyup', (e)=>{
   let nArray = filtrarSearch(data.events,e.target.value)//mando el texto que busco y me devuelve los elementos que coinciden
